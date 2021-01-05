7e4b3e4fe4d5a464c99206c991e79760ca65cf19
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-05 16:38:22 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,15 +1,17 @@
-#include<iostream>
+##include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double ini_loan , int_rate , amount ;
 	cout << "Enter initial loan: ";
+    cin >> ini_loan;
 	cout << "Enter interest rate per year (%): ";
+    cin >> int_rate;
 	cout << "Enter amount you can pay per year: ";
+    cin >> amount;
+    
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +19,31 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+
+    int year = 1;
+    double interest;
+    double total , new_balance;
+    double prev_balance = ini_loan;
+    double payment = amount;
+
+    for(int i = 0;prev_balance > 0;i++){
+        interest = (int_rate*prev_balance)/100;
+        total = prev_balance + interest;
+
+        if(total < payment){
+            payment = total;
+        }
+        new_balance = total - payment;
+
+        cout << setw(13) << left << setprecision (2) << year++;
+        cout << setw(13) << left << setprecision (2) << fixed << prev_balance;
+        cout << setw(13) << left << setprecision (2) << fixed << interest;
+        cout << setw(13) << left << setprecision (2) << fixed << total;
+        cout << setw(13) << left << setprecision (2) << fixed << payment;
+        cout << setw(13) << left << setprecision (2) << fixed << new_balance << endl;
+        prev_balance = new_balance;
+        
+    }
+    return 0;
+}            
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
-	return 0;
-}
