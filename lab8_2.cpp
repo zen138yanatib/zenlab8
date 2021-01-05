077d4a154bee1db811e2520b0044aa5ecd8bf1f4
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-01-05 17:19:04 +0700

    Update lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x,int y){
+    if(x > 0 && y > 0){
+        for(int i = 0;i < x;i++){
+            for(int j = 0;j < y;j++){
+                cout << 'O';
+            }
+            cout << endl;
+        }
+    }else{
+        cout << "Invalid input";
+    }
+}
 
 int main(){
 	
